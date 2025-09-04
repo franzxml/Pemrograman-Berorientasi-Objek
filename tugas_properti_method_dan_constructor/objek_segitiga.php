390185e686b33915e94a9fabafb42601ef42b2f9
franzxml <ignfranzx@gmail.com>
2025-09-04 22:43:35 +0700

    Mengubah

--- tugas_properti_method_dan_constructor/objek_segitiga.php
+++ tugas_properti_method_dan_constructor/objek_segitiga.php
@@ -2,8 +2,17 @@
 require_once "class_segitiga.php";
 
 $segitiga1 = new Segitiga(10, 8, 6, 8, 6);
-echo $segitiga1->tampilkanInfo();
 
 $segitiga2 = new Segitiga(5, 4.33, 5, 5, 5);
+
+?><!DOCTYPE html>
+<html lang="id">
+<head><meta charset="utf-8"><title>Info Segitiga</title></head>
+<body>
+<pre><?php
+echo $segitiga1->tampilkanInfo();
+echo "\n";
 echo $segitiga2->tampilkanInfo();
-?>
+?></pre>
+</body>
+</html>
