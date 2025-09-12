055367413d332168e8b011b173a3abf8d5197176
franzxml <ignfranzx@gmail.com>
2025-09-12 17:23:30 +0700

    Memperbarui

--- pertemuan_5/buat_akun.php
+++ pertemuan_5/buat_akun.php
@@ -19,9 +19,9 @@ $akun_user1->username = "franzxml";
 $akun_user1->password = "1sampai8";
 $akun_user1->email = "franspunyaemail@email.com";
 
-$akun_user2->username = "paundrajmk";
+$akun_user2->username = "paundrajkk";
 $akun_user2->password = "12345678";
-$akun_user2->email = "kocak-kocak@jmk.com";
+$akun_user2->email = "kocak-kocak@jkk.com";
 
 echo $akun_user1->login();
 echo "\n";
